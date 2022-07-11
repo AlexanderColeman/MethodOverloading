d543d4c97886860ddb1fc8116c503d773ad19bc1
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-07-11 11:48:41 -0500

    Add project files.

---
+++ MethodOverloading/Method.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class Method
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool isTrue)
+        {
+            int sum = x + y;
+
+            if (isTrue && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            if (isTrue && sum == 1)
+            {
+                return $"1 dollar";
+            }
+            return "boolean was false";
+        }
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,5 @@
+﻿using MethodOverloading;
+
+Console.WriteLine(Method.Add(1, 1));
+Console.WriteLine(Method.Add(1.5, 1.5));
+Console.WriteLine(Method.Add(0, 1, true)); 
